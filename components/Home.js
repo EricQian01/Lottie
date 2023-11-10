dde39972c1b965f9d75bd1ad32046573fe34b9d5
EricQian01 <ericqian1203@gmail.com>
2023-11-10 14:18:59 -0800

    2nd push

--- components/Home.js
+++ components/Home.js
@@ -1,29 +1,7 @@
-import React, { useEffect } from 'react';
-import Lottie from 'lottie-react';
+import React from "react";
+import Lottie from "lottie-react";
 import homeAnimation from "@/assets/Home.json";
-'@/components/home';
-const Home = () => {
-    useEffect(() => {
-        // Check if we're on the client side before using Lottie
-        if (typeof window !== 'undefined') {
-            const lottieAnimation = Lottie.loadAnimation({
-                container: document.getElementById('lottie-container'),
-                renderer: 'svg',
-                loop: true,
-                autoplay: true,
-                animationData: animationData,
-            });
 
-            // Optionally, you can handle cleanup when the component is unmounted
-            return () => lottieAnimation.destroy();
-        }
-    }, []); // Run this effect only once on mount
-
-    return (
-        <div id="lottie-container" style={{ width: '100%', height: 'auto' }}>
-            {/* Placeholder for Lottie animation */}
-        </div>
-    );
-};
+const Home = () => <Lottie animationData={homeAnimation} loop={true} />;
 
 export default Home;
